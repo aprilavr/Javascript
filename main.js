487d4e9baae33ab7ec7a34f4d3bcb2296fa9b68f
aprilavr <avanrave@stumail.jccc.edu>
2019-09-09 18:54:11 -0500

    Add files via upload
    Javascript for web work: functions for image carousel, interactive navbar

--- main.js
+++ main.js
@@ -1,14 +1,35 @@
-//function for collapsible sections	
+// Image Carousel JavaScript //
+var slideIndex = 0;
 
-var coll = document.getElementsByClassName("collapsible");
-for (var i = 0; i < coll.length; i++) {
-	coll[i].addEventListener("click", function() {
-		this.classList.toggle("active");
-		var content = this.nextElementSibling;
-		if(content.style.maxHeight) {
-			content.style.maxHeight = null;
-		} else {
-			content.style.maxHeight = content.scrollHeight + "px";
-		}
-	}, false);
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("dot");
+		for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex>slides.length) {slideIndex = 1} 
+	    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block"; 
+	dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 8000); // Change image every 8 seconds
+}
+//event listener for image carousel
+window.addEventListener("load",showSlides,false);
+
+
+
+//navbar scripts
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "160px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
 }
